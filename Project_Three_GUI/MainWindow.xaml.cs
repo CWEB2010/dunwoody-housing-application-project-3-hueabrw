f87e02cdd553ee3cbfdba852f1b19a21a65d1795
Abraham <abrhuerta@gmail.com>
2019-03-19 14:23:08 -0500

    saving progress Added a new window

--- Project_Three_GUI/MainWindow.xaml.cs
+++ Project_Three_GUI/MainWindow.xaml.cs
@@ -23,11 +23,67 @@ namespace Project_Three_GUI
         public MainWindow()
         {
             InitializeComponent();
+            invalidCredentials.Visibility = Visibility.Hidden;
         }
-
+        private void OnMouseCapture(object sender, RoutedEventArgs e)
+        {
+            var textBox = e.OriginalSource as TextBox;
+            if (textBox != null)
+            {
+                textBox.SelectAll();
+            }
+            else
+            {
+                var passBox = e.OriginalSource as PasswordBox;
+                if (passBox != null)
+                {
+                    passBox.SelectAll();
+                }
+            }
+        }
+        private void OnKeyboardFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = e.OriginalSource as TextBox;
+            if (textBox != null)
+            {
+                textBox.SelectAll();
+            }
+            else
+            {
+                var passBox = e.OriginalSource as PasswordBox;
+                if (passBox != null)
+                {
+                    passBox.SelectAll();
+                }
+            }
+        }
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Return)
+            {
+                Login();
+            }
+        }
+        private void LoginButton_Click(Object sender, RoutedEventArgs e)
+        {
+            Login();
+        }
+        private void Login()
+        {
+            if (Username.Text == "home" && Password.Password == "1234")
+            {
+                SelectionWindow newWindow = new SelectionWindow();
+                newWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                invalidCredentials.Visibility = Visibility.Visible;
+            }
         }
     }
 }
