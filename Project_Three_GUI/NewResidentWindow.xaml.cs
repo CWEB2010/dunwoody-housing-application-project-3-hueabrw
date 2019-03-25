a69ad82dd7fd12d0fd53f439bbe0172260b1a599
Abraham Huerta <Abrhuerta@gmail.com>
2019-03-24 21:42:51 -0500

    not working :(

--- Project_Three_GUI/NewResidentWindow.xaml.cs
+++ Project_Three_GUI/NewResidentWindow.xaml.cs
@@ -21,19 +21,22 @@ namespace Project_Three_GUI
     /// </summary>
     public partial class NewResidentWindow : Window
     {
+        List<StudentResident> students;
         public NewResidentWindow()
         {
             InitializeComponent();
-            List<ScholarshipStudent> students = LoadData();
+            students = LoadData();
             List<string> types = new List<string> { "Student Worker", "Student Athlete", "Scholarship Student" };
             this.studentTypeBox.ItemsSource = types;
+            this.floorNumebrBox.ItemsSource = new List<int> { 1,2,3};
+            this.roomNumberBox.ItemsSource = new List<int> { 1, 2, 3 };
         }
 
-        private List<ScholarshipStudent> LoadData()
+        private List<StudentResident> LoadData()
         {
             try
             {
-                return JsonConvert.DeserializeObject<List<ScholarshipStudent>>(File.ReadAllText(@"StudentData.json"));
+                return JsonConvert.DeserializeObject<List<StudentResident>>(File.ReadAllText(@"StudentData.json"));
             }
             catch(Exception e)
             {
@@ -60,7 +63,80 @@ namespace Project_Three_GUI
 
         private void StudentTypeBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if(this.studentTypeBox.SelectedItem.ToString() == "Student Worker")
+            {
+                this.MonthlyHoursBox.IsEnabled = true;
+                this.MonthlyHoursText.Foreground = Brushes.Black;
+            }
+            else
+            {
+                this.MonthlyHoursBox.IsEnabled = false;
+                this.MonthlyHoursText.Foreground = Brushes.Gray;
+            }
+            SetButton();
+        }
+
+        private void AddResidentButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Made it");
+            if (studentTypeBox.SelectedItem.ToString() == "Student Worker")
+            {
+                this.students.Add(new StudentWorker(students.Count().ToString(), this.firstNameBox.Text, this.lastNameBox.Text, (int)this.roomNumberBox.SelectedItem, (int)this.floorNumebrBox.SelectedItem, Convert.ToInt32(this.MonthlyHoursBox.Text)));
+            }
+            else if (studentTypeBox.SelectedItem.ToString() == "Student Athlete")
+            {
+                this.students.Add(new StudentAthlete(students.Count().ToString(), this.firstNameBox.Text, this.lastNameBox.Text, (int)this.roomNumberBox.SelectedItem, (int)this.floorNumebrBox.SelectedItem));
+            }
+            else if (studentTypeBox.SelectedItem.ToString() == "Scholarship Student")
+            {
+                this.students.Add(new ScholarshipStudent("0", this.firstNameBox.Text, this.lastNameBox.Text, Convert.ToInt32(this.roomNumberBox.SelectedItem.ToString()), Convert.ToInt32(this.floorNumebrBox.SelectedItem.ToString())));
+            }
+            SaveData();
+        }
+
+        private void SaveData()
+        {
+            string jsonData = JsonConvert.SerializeObject(students);
             
+            System.IO.File.WriteAllText(@"StudentData.json", jsonData);
+            this.Close();
+        }
+
+        private void SetButton()
+        {
+            if(studentTypeBox.SelectedItem.ToString() == "Student Worker")
+            {
+                AddResidentButton.IsEnabled = (studentTypeBox.SelectedItem != null && firstNameBox.Text != "" && lastNameBox.Text != "" && floorNumebrBox.SelectedItem != null && roomNumberBox.SelectedItem != null && MonthlyHoursBox.Text != "");
+            }
+            else
+            {
+                AddResidentButton.IsEnabled = (studentTypeBox.SelectedItem != null && firstNameBox.Text != "" && lastNameBox.Text != "" && floorNumebrBox.SelectedItem != null && roomNumberBox.SelectedItem != null);
+            }
+        }
+
+        private void FirstNameBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SetButton();
+        }
+
+        private void LastNameBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SetButton();
+        }
+
+        private void FloorNumebrBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SetButton();
+        }
+
+        private void RoomNumberBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SetButton();
+        }
+
+        private void MonthlyHoursBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SetButton();
         }
     }
 }
